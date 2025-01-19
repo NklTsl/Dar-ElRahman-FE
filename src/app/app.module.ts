bac2f046a3a0b97418ca1eb1d31f0451e302f5d6
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2025-01-19 22:09:32 +0300

    add Kotab page with table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { DataTableComponent } from './data-table/data-table.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    UserDetailsComponent,
+    DataTableComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pagination/pagination.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.css'],
+})
+export class PaginationComponent {
+  @Input() currentPage: number = 1; // Receive current page from the parent
+  @Input() totalPages: number = 1; // Receive total pages from the parent
+  @Output() pageChange: EventEmitter<number> = new EventEmitter<number>(); // Emit changes to the parent
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.pageChange.emit(this.currentPage - 1);
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.pageChange.emit(this.currentPage + 1);
+    }
+  }
+}
