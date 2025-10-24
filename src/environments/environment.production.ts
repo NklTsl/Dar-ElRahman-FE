2aa090b49a3435b463e9c456285eba2bdaf5b820
Hamada Abdrabou <hamadaabdrabou96@gmail.com>
2025-10-24 10:24:03 +0400

    Updating backend url for cors origin.

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   env: 'prod',
   production: true,
-  memoApiUrl: 'http://the-most-merciful-servants.railway.internal/memo-api'
+  memoApiUrl: 'https://the-most-merciful-servants-production.up.railway.app/memo-api'
 };
